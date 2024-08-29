e4b50bd3a9d6ce286d3fad3f2ed34483e4eda3b2
McGinnis <mmcginnis1@una.edu>
2024-08-29 13:12:27 -0500

    added my docs folder

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ console.log('I am on a node server');
 
 const express = require('express')
 const app = express()
+app.use(express.static('./public/'))
 
 app.get('/', function (req, res) {
-  res.send('Hello Node from Express on Local Dev Box')
+  // res.send('Hello Node from Express on Local Dev Box')
+  res.sendFile('index.html');
 })
 
 app.listen(5000)
